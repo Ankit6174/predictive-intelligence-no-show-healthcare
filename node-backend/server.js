27538cd39f6e3ded1ade2a81de171272bd12afef
Ankit Ahirwar <ahirwarankit2345@gmail.com>
2025-08-22 12:04:34 +0530

    error: atlas

--- node-backend/server.js
+++ node-backend/server.js
@@ -7,10 +7,10 @@ const userShema = require('./models/contect');
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-app.set('views', path.join(__dirname, 'views'));
 
 dotenv.config();
 
