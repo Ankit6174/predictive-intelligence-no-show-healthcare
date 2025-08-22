1bdbc31a2145ad54be82ea335df2ab550e42b616
Ankit Ahirwar <ahirwarankit2345@gmail.com>
2025-08-22 12:00:38 +0530

    error: solve atlas

--- node-backend/config/contectDB.js
+++ node-backend/config/contectDB.js
@@ -1,13 +1,9 @@
 const mongoose = require('mongoose');
 
 const contectDB = async() => {
-    try {
-        const conntedContect = process.env.MONGO_URL || 'mongodb://localhost:27017/test';
-        await mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/test');
-        console.log(`Connected to -> ${conntedContect}`);
-    } catch {
-        console.log("Failed to connect");
-    }
+    mongoose.connect(process.env.MONGO_URL)
+        .then(() => console.log('Connected to MongoDB Atlas'))
+        .catch(err => console.error('Error connecting to MongoDB', err));
 };
 
 module.exports = contectDB;
